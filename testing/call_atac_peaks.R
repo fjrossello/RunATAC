e18b69388ae03dc67264ca2eab102527fb8d471c
SamBuckberry <sambuckberry@gmail.com>
2016-08-25 18:01:54 +0800

    still testing peak calling

--- testing/call_atac_peaks.R
+++ testing/call_atac_peaks.R
@@ -13,19 +13,19 @@ chrom_info <- fetchExtendedChromInfoFromUCSC(genome = "mm10")
 atac_gr <- read_atac_pos(bam_file = "~/Desktop/atac_MEF_chr16_test.bam")
 length(atac_gr)
 
-read_width <- 25
-atac_gr <- atac_gr + read_width
+#read_width <- 25
+#atac_gr <- atac_gr + read_width
 
-df <- data.frame(atac_gr)
-colnames(df) <- c("chr", "start", "end", "strand")
+# df <- data.frame(atac_gr)
+# colnames(df) <- c("chr", "start", "end", "strand")
+# 
+# rd <-as(atac_gr, "RangedData") 
+# pks <- bayespeak(treatment = df, bin.size = 100, chr = "chr16",
+#                  start = 1, end = 98207768)
+# 
+# sp <- summarise.peaks(pks)
 
-rd <-as(atac_gr, "RangedData") 
-pks <- bayespeak(treatment = df, bin.size = 100, chr = "chr16",
-                 start = 1, end = 98207768)
-
-sp <- summarise.peaks(pks)
-
-bin_size <- 100
+bin_size <- 50
 assembly <- "mm10"
 blacklist <- read_bed("~/polo_iPSC/resources/mm10_blacklist_regions.bed")
 
@@ -36,6 +36,7 @@ call_atac_peaks <- function(atac_gr...){
         bin_gr <- bin_gr[seqnames(bin_gr) %in% c("chr16")]
         
         model <- chromstaR::callPeaksUnivariate(binned.data = bin_gr,
+                                                #read.cutoff.quantile = 0.99, 
                                                 keep.posteriors = TRUE,
                                                 prefit.on.chr = "chr16")
         
